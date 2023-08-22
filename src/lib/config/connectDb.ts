6f594eddfd63a9d1e4751f8da8bf92d78e675e8e
Mkparu <mkparusomtochi26@gmail.com>
2023-08-22 21:30:00 +0100

    chore(lib/config): add types to config.

--- src/lib/config/connectDb.ts
+++ src/lib/config/connectDb.ts
@@ -1,12 +1,19 @@
-import mongoose from 'mongoose'
-
+import mongoose,{Connection} from 'mongoose'
+// declare on the global namespace 
+declare global {   
+      var mongoose: {
+        connection: Connection | null;
+        promise: Promise<Connection> | null;
+        }
+  }
+// Cache the  connections to avoid maxing my Concurrent limit.
 const connectionUrl = process.env.MONGO_URL;
 let cached = global.mongoose;
 if(!cached){
     global.mongoose = {connection:null, promise: null};
     cached = global.mongoose
 }
-const connectDb = async () => 
+const connectDb: () => Promise<mongoose.Connection | undefined> = async () => 
 {
     console.log('in connect db')
     try{
@@ -18,7 +25,7 @@ const connectDb = async () =>
             return cached.connection;
         }
         if(!cached.promise){
-           cached.promise = await mongoose.connect(connectionUrl,{bufferCommands: false})
+           cached.promise   =  await mongoose.connect(connectionUrl,{bufferCommands: false}) as unknown as  Promise<Connection>;
            console.log("Successfully connected to mongo db.")
         }
         cached.connection = await cached.promise;
