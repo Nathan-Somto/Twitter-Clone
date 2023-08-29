affce519b63cd65d55c144604f1e1d355b8a499d
Mkparu <mkparusomtochi26@gmail.com>
2023-08-29 20:59:31 +0100

    chore(store): add updated reducers to store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
-import themeReducer from '@/features/theme/themeSlice'
-import tweetReducer from '@/features/tweets/tweetsSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "@/features/theme/themeSlice";
+import tweetReducer from "@/features/tweets/tweetsSlice";
+import filterTweetsReducer from "@/features/filterTweets/filterTweetsSlice";
+import userReducer from "@/features/users/usersSlice";
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
-    tweets: tweetReducer
+    tweets: tweetReducer,
+    filterTweets: filterTweetsReducer,
+    user: userReducer,
   },
-})
+});
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
