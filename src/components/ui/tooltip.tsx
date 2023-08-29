13f3eb6d79a3d391d8e512b0026b4a7b384b2350
Mkparu <mkparusomtochi26@gmail.com>
2023-08-29 20:39:19 +0100

    feat(ui): create tooltip component.

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useLayoutEffect } from "react";
+import { useIsomorphicLayoutEffect } from "@/hooks/useIsomorphicLayoutEffect";
+import React, { useRef } from "react";
 
 type Props = React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
@@ -7,15 +8,21 @@ type Props = React.DetailedHTMLProps<
   children: React.ReactNode;
   distanceFromItem?: number; // Distance in pixels
 };
-
-function Tooltip({ children, distanceFromItem = 10, ...props }: Props) {
-  const tooltipRef = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    if (tooltipRef.current) {
+/**
+ * @todo make component reusable for other positions like top, left and bottom.
+ * @param param0 
+ * @description a component that renders a tooltip containing information.
+ * @returns 
+ */
+function Tooltip({ children, distanceFromItem = 6, ...props }: Props) {
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
+  // this hook is used instead of useLayout effect due to ssr read issue here
+  // https://github.com/facebook/react/issues/14927
+  useIsomorphicLayoutEffect(() => {
+    if (tooltipRef.current !== null) {
       // Calculate the right spacing based on distance and tooltip size
       const tooltipWidth = tooltipRef.current.offsetWidth;
-      tooltipRef.current.style.right = `calc(- ${distanceFromItem}px - ${tooltipWidth}px)`;
+      tooltipRef.current.style.right = `calc(${-distanceFromItem}px - ${tooltipWidth}px)`;
     }
   }, [distanceFromItem]);
 
@@ -23,7 +30,7 @@ function Tooltip({ children, distanceFromItem = 10, ...props }: Props) {
     <div
       {...props}
       ref={tooltipRef}
-      className=" 
+      className={` 
     absolute 
     z-[10] 
     -top-1 
@@ -39,7 +46,7 @@ function Tooltip({ children, distanceFromItem = 10, ...props }: Props) {
     after:border-t-transparent
     after:border-b-transparent
     after:border-l-transparent 
-    rounded-[6px] transition-opacity  bg-[rgba(0,0,0,0.75)] px-3 py-2  duration-500"
+    rounded-[6px] transition-opacity  bg-[rgba(0,0,0,0.75)] px-3 py-2  duration-500 ${props.className}`}
     >
       <span className="text-primaryWhite subtle-semibold">{children}</span>
     </div>
