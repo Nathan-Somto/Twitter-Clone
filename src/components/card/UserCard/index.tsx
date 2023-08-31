905953c10b071c1b248ba29fe56ff24d15a5c016
Mkparu <mkparusomtochi26@gmail.com>
2023-08-31 21:47:24 +0100

    feat(components/usercard): add user follow and unfollow functionality.

--- src/components/card/UserCard/index.tsx
+++ src/components/card/UserCard/index.tsx
@@ -1,52 +1,144 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import Link from 'next/link'
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useDispatch } from "react-redux";
+import { CustomSession } from "@/types";
+import axios from "axios";
+import { toggleFollow } from "@/features/users/usersSlice";
+import { toast } from "@/components/ui/use-toast";
 
-interface Props {
-  id: string;
+type Props = {
+  _id: string;
   name: string;
   username: string;
-  imgUrl: string;
-  actionType: "View Profile" | "Follow";
-  followers?:string[]
-}
+  profileImgUrl: string;
+  followers?: string[];
+};
 
-function UserCard({ id, name, username, imgUrl, actionType,followers }: Props) {
+function UserCard({ _id, name, username, profileImgUrl, followers }: Props) {
   const router = useRouter();
   const [isFollowing, setIsFollowing] = useState(false);
-  const viewProfile = actionType === "View Profile";
- function handleAction() {
-    if(viewProfile){
-        return router.push(`/profile/${id}`)
+  const [loading, setLoading] = useState(false);
+  const { data: session } = useSession();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (followers) {
+      setIsFollowing(
+        followers?.find(
+          (user_id) => user_id === (session as CustomSession)?.user?.id
+        ) !== undefined
+      );
     }
+  }, [followers, session]);
+  const isCurrentUserPage =
+    router.query?.id === (session as CustomSession)?.user?.id;
+  const handleAction = useCallback(async () => {
     // call the api and follow user.
- }
+    setLoading(true);
+    try {
+      if (!(session as CustomSession)?.user?.id) {
+        throw new Error("user not Logged in.");
+      }
+      setIsFollowing((prevState) => !prevState);
+      if (!isFollowing && !isCurrentUserPage) {
+       followers?.push((session as CustomSession)?.user?.id ?? "");
+      } 
+      const response = await axios.put(`/api/users/${_id}/follow`, {
+        targetUserId: (session as CustomSession)?.user?.id,
+      });
+      console.log(response.data);
+      if (response.data?.status === "success") {
+        // if it is the user's (logged in) profile page dispatch this actions.
+        /**
+         *  think about it if it is the logged in user page an he/she is viewing the cards on the modal
+         *  and unfollows and follows, when he/she exits the data should be updated realtime.
+         * */
+        if (isCurrentUserPage) {
+          if (isFollowing) {
+            // unfollow user.
+            dispatch(
+              toggleFollow({
+                _id: (session as CustomSession)?.user?.id as string,
+                remove: true,
+              })
+            );
+          } else {
+            // follow user.
+            dispatch(
+              toggleFollow({
+                _id: (session as CustomSession)?.user?.id as string,
+                remove: false,
+              })
+            );
+          }
+        }
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(err);
+        if(isFollowing){
+          followers?.pop();
+        }
+        else{
+          followers?.push((session as CustomSession)?.user?.id ?? "");
+        }
+        setIsFollowing((prevState) => !prevState);
+        toast({
+          description: `failed to ${
+            isFollowing ? "unfollow" : "follow"
+          } ${username}`,
+          variant: "destructive",
+        });
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [
+    _id,
+    dispatch,
+    followers,
+    isCurrentUserPage,
+    isFollowing,
+    session,
+    username,
+  ]);
   return (
-    <article className='flex min-h-[60px] justify-between gap-4   max-xs:p-4 flex-row items-center'>
-      <div className='flex flex-1 items-start justify-start gap-3 xs:items-center'>
-        <div className='relative h-12 w-12'>
-          <Image
-            src={imgUrl}
-            alt='user_logo'
-            fill
-            className='rounded-full object-cover'
-          />
-        </div>
-
-        <Link href={`/profile/${id}`} className='flex-1 w-[60px]'>
-          <h4 className='base-semibold text-dark1 dark:text-light1 text-clip w-full'>{name}</h4>
-          <p className={`${actionType === "View Profile" ? "subtle-medium":"small-medium"}small-medium text-dark2 dark:text-light2`}>@{username}</p>
-          {followers && <p className='small-semibold text-dark2 dark:text-light2 mt-1'>{followers.length} Followers</p>}
+    <article className="flex min-h-[60px] justify-between gap-4   max-xs:p-4 flex-row items-center">
+      <div className="flex flex-1 items-start justify-start gap-3 xs:items-center">
+        <Link href={`/profile/${_id}`}>
+          <div className="relative h-12 w-12">
+            <Image
+              src={profileImgUrl}
+              alt="user_logo"
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
+        </Link>
+        <Link href={`/profile/${_id}`} className="flex-1 w-[60px]">
+          <h4 className="base-semibold text-dark1 dark:text-light1 text-clip w-full">
+            {name}
+          </h4>
+          <p className={`small-medium text-dark2 dark:text-light2`}>
+            @{username}
+          </p>
+          {followers && (
+            <p className="small-semibold text-dark2 dark:text-light2 mt-1">
+              {followers.length} Follower{followers.length > 1 ? "s" : ""}
+            </p>
+          )}
         </Link>
       </div>
-
       <Button
-        variant={'outline'}
+        variant={"outline"}
         onClick={handleAction}
+        disabled={loading}
+        className="!disabled:cursor-not-allowed"
       >
-        {viewProfile ? 'View': isFollowing ? 'Following': 'Follow'}
+        {isFollowing ? "Unfollow" : "Follow"}
       </Button>
     </article>
   );
