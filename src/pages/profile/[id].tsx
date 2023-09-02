eef2ca8dc3033e1fb0570bec1d885ca3cb156967
Mkparu <mkparusomtochi26@gmail.com>
2023-09-02 21:53:17 +0100

    feat(pages/profile): fetch user profile data from server.

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,40 +1,92 @@
 import Header from "@/components/common/Header";
 import Profile from "@/components/common/Profile";
-import React, { useState } from "react";
-import dummyData from "@/components/card/TweetCard/fakedata.json";
-import TweetCard from "@/components/card/TweetCard";
-type Props = {};
-function TweetSection<T extends typeof dummyData>({ data }: { data: T }) {
-  return (
-    <section className="border-t dark:border-t-dark4">
-      {data.length ? (
-        data.map((data) => <TweetCard {...data} key={data._id} />)
-      ) : (
-        <p className="h4-semibold">No Data</p>
-      )}
-    </section>
-  );
-}
-function ProfilePage({}: Props) {
-  const [data, setData] = useState(dummyData);
-  const [filterData, setFilterData] = useState<typeof dummyData>([]);
-  function filterHandler(option: string) {
-    if (option === "Tweets") {
-      setFilterData([]);
-    } else if (option === "Media") {
-      const mediaData = data.filter((item) => item.imgUrls.length);
-      setFilterData([...mediaData]);
-    } else {
-      const replyData = data.filter((item) => item.comments.length);
-      setFilterData([...replyData]);
+import React, { useEffect } from "react";
+import { Profile as IProfile, setUser } from "@/features/users/usersSlice";
+import UserTweets from "@/components/common/Profile/UserTweets";
+import { useDispatch } from "react-redux";
+import { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+import Layout from "@/components/layout";
+import { setSelectedFilter } from "@/features/filterTweets/filterTweetsSlice";
+import axios from "axios";
+import { CustomSession } from "@/types";
+import Seo from "@/components/seo";
+type Props = {
+  user: IProfile;
+};
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  res,
+  query,
+}) => {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session?.user) {
+      return {
+        redirect: {
+          destination: "/sign-in",
+          permanent: false,
+        },
+      };
     }
+    if (!(session as CustomSession)?.user?.onBoarded) {
+      return {
+        redirect: {
+          destination: "/onboarding",
+          permanent: false,
+        },
+      };
+    }
+    const profileData = await axios.get(
+      `${process.env.SITE_URL}/api/users/${query.id}`
+    );
+
+    if (!profileData.data?.user) {
+      return {
+        notFound: true,
+      };
+    }
+    console.log(profileData.data?.user);
+    return {
+      props: {
+        user: profileData.data.user,
+      },
+    };
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+    }
+    return {
+      notFound: true,
+    };
   }
+};
+function ProfilePage({ user }: Props) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setUser(user));
+    dispatch(setSelectedFilter("Tweets"));
+  }, [dispatch, user]);
   return (
-    <div className="border-r dark:border-r-dark4  h-screen overflow-auto">
-      <Header titleText="Nathan Somto" subtitleText="6 tweets" />
-      <Profile filterHandler={filterHandler} />
-      <TweetSection data={filterData.length ? filterData : data} />
-    </div>
+    <>
+    <Seo
+      title={`@${user.username + ' on'} X`}
+      description={`check out ${user.username} profile on X.`}
+    />
+    <Layout>
+      <div className="border-r dark:border-r-dark4  h-screen overflow-auto pb-[75px] md:pb-0">
+        <Header
+          titleText={user.displayName}
+          subtitleText={`${user.tweets.length} tweet${
+            user.tweets.length > 1 ? "s" : ""
+          }`}
+        />
+        <Profile />
+        <UserTweets />
+      </div>
+    </Layout>
+    </>
   );
 }
 
