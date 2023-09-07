b2dd234cfdd115c0e0fe591190b984cf994d805f
Mkparu <mkparusomtochi26@gmail.com>
2023-09-07 22:24:43 +0100

    chore(pages/onboarding): add seo component and x logo.

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,9 +1,9 @@
 import AccountProfile from "@/components/form/AccountProfile";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import Loader from '@/components/ui/loader';
 import { CustomSession } from "@/types";
+import Seo from "@/components/seo";
 
 export default function OnBoardingPage() {
   const {data: session, status} = useSession()
@@ -16,11 +16,13 @@ export default function OnBoardingPage() {
  // if user is not logged in go to sign in page
  if(!session){
   router.push('/sign-in');
+  return null;
  }
   
   // if user has onboarded go to home page
   if((session  as CustomSession)?.user?.onBoarded){
     router.push('/home');
+    return null;
   }
   // the bio and profileCover should always be empty because it is assumed when you haven't onBoarded you dont have the data.
   // for edit profile it will be prefilled because we fetch data Server Side
@@ -34,10 +36,21 @@ export default function OnBoardingPage() {
   }
   return (
     <>
+      <Seo
+        title ={`Setup your profile for X @${(session as CustomSession)?.user?.name}`}
+      />
       <nav className="w-full py-4 px-8 mx-auto dark:bg-primaryBlack bg-light3 dark:text-light2 text-dark3">
-        <figure className="h-[30px] w-[30px] hidden md:block relative">
-          <Image src={"/Logo.png"} alt="twitter logo" fill className="object-contain" />
-        </figure>
+        <figure>
+            <svg
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              className="md:h-[30px] md:w-[30px] h-[50px] w-[50px]  dark:fill-primaryWhite fill-primaryBlack "
+            >
+              <g>
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+              </g>
+            </svg>
+          </figure>
       </nav>
       <AccountProfile
         headingText="Setup Profile"
