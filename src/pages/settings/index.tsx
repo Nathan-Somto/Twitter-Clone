a3e72429bbbdc1ff0cb617b0de574626739d813d
Mkparu <mkparusomtochi26@gmail.com>
2023-09-08 18:52:42 +0100

    chore(pages/settings): add settings component to settings page.

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,25 +1,14 @@
-import {
-  Logout,
-  ThemeChanger,
-  DeleteAccount,
-} from "@/components/common/Settings";
-import SideBar from "@/components/layout/SideBar";
+import Settings from "@/components/settings";
 import React from "react";
 import Header from "@/components/common/Header";
+import Layout from "@/components/layout";
 
 function SettingsPage() {
   return (
-    <div className="grid md:grid-cols-[100px_1fr] lg:grid-cols-[275px_1fr] mx-auto h-screen w-full  dark:bg-primaryBlack bg-primaryWhite dark:text-primaryWhite">
-      <SideBar />
-      <main>
-        <Header titleText="Settings" />
-        <section className="w-[90%] mx-auto mt-8 space-y-7">
-          <ThemeChanger />
-          <Logout />
-          <DeleteAccount />
-        </section>
-      </main>
-    </div>
+    <Layout>
+      <Header titleText="Settings" />
+      <Settings />
+    </Layout>
   );
 }
 
